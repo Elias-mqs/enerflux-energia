208db362c9922335ecf6ceacba771ceaf6acf287
Elias Marques <eliasmqs397@gmail.com>
2024-10-23 12:49:31 -0400

    fix:bugs

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,5 +2,5 @@
 export function getBaseUrl() {
   if (typeof window === 'undefined') return ''
   if (process.env.VERCEL_URL) return `${process.env.VERCEL_URL}/api/`
-  return `${process.env.NEXT_PUBLIC_BASE_URL}/api/`
+  // return `${process.env.NEXT_PUBLIC_BASE_URL}/api/`
 }
